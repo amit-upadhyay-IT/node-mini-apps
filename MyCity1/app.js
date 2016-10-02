d16dbf516f6020309dae1bdc0332f000f5bd575b
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-10-02 19:24:10 +0530

    updated MyCity1 and modularised the code using routes.js file

--- MyCity1/app.js
+++ MyCity1/app.js
@@ -3,54 +3,13 @@ var app = exp();
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){// this is mapping the home page
-    res.render('home.ejs', {title:"I Love my city",
-                            headline:"Every city has its own personality"}); // the template file will have data to be changed and the changing data has to be captured
-    // in javascript variables and the values for the changing data will be provided within the {}.
+var handlers = require('./routes/routes.js');
 
-    // the express library will automatically be able to find a file whose name is home.ejs and after that that file will be
-    // shown over here.
+app.get('/', handlers.homeFn);
 
-    // we have only two variables inside our home.ejs file and those are title and headline and these have to be specified in
-    // these {}
-});
 
-app.get('/:city', function(req, res){
 
-    // for city we want to use single template file and have data for 4-5 cities over there. So that's why we except the user to enter something like hostname/london, hostname/newyork
-    // etc. So the / and positional parameter are mentioned in 'city' variable.
-    // So we have to populate the values of title and headline appropriately.
-
-    var cityname = req.params.city;
-
-    var titleValue;
-    var headlineValue;
-    // based on city I'll modify the titleValue and headlineValue
-
-    if (cityname === 'newyork')
-    {
-        titleValue = "New York";
-        headlineValue = "Business capital of the world";
-    }
-    else if (cityname == 'london')
-    {
-	    titleValue = "London";
-        headlineValue = "City of Thames";
-    }
-    else if (cityname == 'newdelhi')
-    {
-	    titleValue = "New Delhi";
-        headlineValue = "Place where I live";
-    }
-    else if (cityname == 'paris')
-    {
-	    titleValue = "Paris";
-        headlineValue = "Place where I never visited";
-    }
-
-    res.render('city.ejs', {title:titleValue,
-    headline:headlineValue});
-});
+app.get('/:city', handlers.cityFn);
 
 var port = process.env.PORT || 4000;
 
