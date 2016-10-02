e0d3e3b5a3538f46bca3b50116da5437c090cbe1
amit-upadhyay-IT <youarefreak1@gmail.com>
2016-10-02 17:04:37 +0530

    updated app.js

--- MyCity1/app.js
+++ MyCity1/app.js
@@ -1,8 +1,9 @@
 var exp = require("express");
-
 var app = exp();
 
-var port = process.env.PORT || 4000; // We are basically doint this for heroku deployment:heroku keeps your port number at a specific location and if you don't specify and if you don't specify and port number then it uses a default port number.
+app.set('view engine', 'ejs');
+
+var port = process.env.PORT || 4000;
 
 app.listen(port, function(){
     console.log("Server is listening on port "+port);
